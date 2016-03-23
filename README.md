624182e0a212e6371c770c3d8991640f0124be39
addinall <addinall@addinall.net>
2016-03-24 07:01:59 +1000

    Initial creation

---
+++ README.md
@@ -0,0 +1,10 @@
+# oo.c
+## ANSI C using an OOD/OOP paradigm
+### March 22 2016- Mark Addinall
+----
+
+Got a new gig writing full stack web interfaces
+to a remote sensing system.  Some, or all of
+the sensor software is in C.  So, after quite a few
+moons, chuck on my K&R hat and code up some yummy
+C objects to play with.
---
+++ oo.c
@@ -0,0 +1,13 @@
+// CAPTAIN SLOG
+// vim: set expandtab tabstop=4 shiftwidth=4 autoindent smartindent:
+// File         : oo.c
+// System       : ANSI C using an OOD/OOP paradigm
+// Date         : March 22 2016
+// Author       : Mark Addinall
+// Synopsis     : Got a new gig writing full stack we interfaces
+//                to a remote sensing system.  Some, or all of
+//                the sensor software is in C.  So, after quite a few
+//                moons, chuck on my K&R hat and code up some yummy
+//                C objects to play with.
+//
+// -------------------------------
